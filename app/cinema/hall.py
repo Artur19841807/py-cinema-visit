64c9c6a3996c4a3f59468d4b8318afea3b8641ba
Artur Neruk <anerukpfk@gmail.com>
2026-07-06 14:59:50 +0300

    fix: remove redundant attributes, fix dublicate files and clear formatting string

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,7 +1,6 @@
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
-        self.hall_number = number
 
     def movie_session(self, movie_name: str, customers: list, cleaning_staff: object) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
