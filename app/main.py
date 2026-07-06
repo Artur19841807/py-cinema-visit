fc06117060b9602504727790c0e338262ea8f07b
Artur Neruk <anerukpfk@gmail.com>
2026-07-06 16:20:51 +0300

    fix: resolve customer syntax error and fix line formatting in main.py

--- app/main.py
+++ app/main.py
@@ -4,17 +4,23 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
-    # write you code here
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
     cleaner_instance = Cleaner(name=cleaner)
     hall_instance = CinemaHall(number=hall_number)
 
     customer_instances = []
     for client_data in customers:
-        customer = Customer(name=client_data["name"], food=client_data["food"])
+        customer = Customer(
+            name=client_data["name"],
+            food=client_data["food"])
         customer_instances.append(customer)
-        CinemaBar.sell_product(product=customer.food, customer=customer)
-
+        CinemaBar.sell_product(product=customer.food,
+                               customer=customer)
     hall_instance.movie_session(
         movie_name=movie,
         customers=customer_instances,
